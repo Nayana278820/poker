0b7c79f9901ca088aa013495ac1a870362826985
Nayana278820 <110936505+Nayana278820@users.noreply.github.com>
2022-12-03 13:33:06 -0500

    other functs

--- main.cpp
+++ main.cpp
@@ -64,14 +64,75 @@ int main(){
     cout << "The cards on table are: ";
 	players->dealHand(3, "dealer"); // deal cards to the players
 
-}
+    // do it until we have 5 cards down or until everyone but one player won
+    while(players->numCards() < 6){
+		// Ask each player for first move;
+		//int i;
+		//for(i = 1; i < numPlayers + 1; i++){
+			cout << "\nPlayer,  what is your move (fold = f, raise = r, check = c, deal = d)? ";
+			cin >> move;
+
+			if(move == 'f'){
+				players->fold();
+				numFolds ++;
+			}
+			else if(move == 'r'){
+				cout << "How much is your raise? ";
+				cin >> raiseAmnt;
+				//players.raise(i, raiseAmnt); // NEED TO ADD THIS TO THE READER FILE- SECOND PARAMETER!!!
+				//money(raiseAmnt, s);
+				//totalmoney(raiseAmnt);
+				break; // if someone raises, everyone else needs to go again to try to match the raise
+			}
+            else if(move == 'c'){
+				//players.check();
+			}
+		}
+
+		/*if(move == 'r'){
+			// for initalizer: start with next player (circular way)
+			// for condition- stop when get back to that person
+			// if they want to meet
+			char raise;
+			for(i = (i + 1) % numPlayers; i != i; i++){
+				cout << "Player " << i << ", would you like to meet the raise (y/n)? ";
+				cin >> raise;
+				if(raise == 'y'){
+					// not the same as raising but same logic so user that same function
+					//players.raise(i, raiseAmnt);
+					money(raiseAmnt, s, i);
+					totalmoney(raiseAmnt);
+				}
+				else if(raise == 'n'){
+					players.fold(i);
+				}
+			}
+		}*/
+		/*if(numFolds == 1){
+		//	players->win(); // in function: when fold, give cards back- then need to check who has cards left (prob first thing to check)
+			cout << "you folded";
+            return;
+		}
+		*/
+	//	if(players->numCards() != 5){
+			players->dealHand(1, "player");
+	//	}
+    return 1;
+	}
+
+	//players->win(); // cout who won and how much they won
+//}
 	
 poker::poker() {
     playerOneMoney = 0;
 };
 
-void displayCards(){
+void poker::fold(){ // lay down your cards and stop playing
+    playerOne.clear();
+ }
 
+int poker::numCards(){
+    return playerOne.size();
 }
 
  string poker::deal (){
@@ -172,64 +233,3 @@ void poker::dealHand(int numCards, string player){
 }
 	// need a display cards function to display the cards that the dealer puts down
 	//players.displayCards(3); // parameter: how many cards we need displayed
-
-
-	// do it until we have 5 cards down or until everyone but one player won
-	/*
-    while(players->numCards() < 6){
-		// Ask each player for first move;
-		//int i;
-		//for(i = 1; i < numPlayers + 1; i++){
-			cout << "Player,  what is your move (fold = f, raise = r, check = c, deal = d)? ";
-			cin >> move;
-
-			if(move == 'f'){
-				players->fold();
-				numFolds ++;
-			}
-			else if(move == 'r'){
-				cout << "How much is your raise? ";
-				cin >> raiseAmnt;
-				//players.raise(i, raiseAmnt); // NEED TO ADD THIS TO THE READER FILE- SECOND PARAMETER!!!
-				//money(raiseAmnt, s);
-				//totalmoney(raiseAmnt);
-				break; // if someone raises, everyone else needs to go again to try to match the raise
-			}
-            else if(move == 'c'){
-				//players.check();
-			}
-		}
-
-		/*if(move == 'r'){
-			// for initalizer: start with next player (circular way)
-			// for condition- stop when get back to that person
-			// if they want to meet
-			char raise;
-			for(i = (i + 1) % numPlayers; i != i; i++){
-				cout << "Player " << i << ", would you like to meet the raise (y/n)? ";
-				cin >> raise;
-				if(raise == 'y'){
-					// not the same as raising but same logic so user that same function
-					//players.raise(i, raiseAmnt);
-					money(raiseAmnt, s, i);
-					totalmoney(raiseAmnt);
-				}
-				else if(raise == 'n'){
-					players.fold(i);
-				}
-			}
-		}*/
-/*
-		if(numFolds == 1){
-		//	players->win(); // in function: when fold, give cards back- then need to check who has cards left (prob first thing to check)
-			cout << "you folded";
-            return;
-		}
-		
-		if(players->numCards() != 5){
-			players->dealHand(1);
-		}
-	}
-    */
-
-	//players->win(); // cout who won and how much they won
